b09ab7e3ae83da060bf2cc8f2db8f2a55109d9b2
Axel Bodart <axel.bodart@gmail.com>
2014-01-03 15:52:10 +0100

    Fix code style : add missing semicolons and remove extra ones, normalize spacings, remove unneeded variables and duplicate ones

--- src/dmak.js
+++ src/dmak.js
@@ -1,22 +1,22 @@
  ;(function () {
 
-    "use strict"
+    "use strict";
 
     // Create a safe reference to the DrawMeAKanji object for use below.
-    var Dmak = function(text, options) {
+    var Dmak = function (text, options) {
         this.text = text;
-        this.options  = extend(Dmak.options, options);
+        this.options = extend(Dmak.options, options);
         this.strokes = [];
         this.papers = [];
-        this.pointer  = 0;
+        this.pointer = 0;
         this.timeouts = [];
 
-        if(!this.options.skipLoad) {
+        if (!this.options.skipLoad) {
             var loader = new DmakLoader(this.options.uri);
-            loader.load(text, function(strokes) {
+            loader.load(text, function (strokes) {
                 this.setStrokes(strokes);
                 this.options.loaded(this.strokes);
-                if(this.options.autoplay) {
+                if (this.options.autoplay) {
                     this.render();
                 }
             }.bind(this));
@@ -27,42 +27,45 @@
     Dmak.VERSION = '0.1';
 
     Dmak.options = {
-        uri : '',
-        skipLoad : false,
-        autoplay : true,
-        height : 109,
-        width : 109,
-        step : 0.03,
-        element : 'draw',
-        stroke : {
-            animated : true,
-            attr : {
+        uri: '',
+        skipLoad: false,
+        autoplay: true,
+        height: 109,
+        width: 109,
+        step: 0.03,
+        element: 'draw',
+        stroke: {
+            animated: true,
+            attr: {
                 'active': '#F92672',
-                'stroke' : "#272822",
-                'stroke-width' : 3,
-                'stroke-linecap' : 'round',
-                'stroke-linejoin' : 'round'
+                'stroke': "#272822",
+                'stroke-width': 3,
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round'
             }
         },
-        grid : {
+        grid: {
             show: true,
-            attr : {
+            attr: {
                 'stroke': '#CCCCCC',
-                'stroke-width' : 0.5,
-                'stroke-dasharray' : "--"
+                'stroke-width': 0.5,
+                'stroke-dasharray': "--"
             }
         },
-        loaded : function(){},
-        erased : function(){},
-        drew:  function(){}
+        loaded: function () {
+        },
+        erased: function () {
+        },
+        drew: function () {
+        }
     };
 
     Dmak.fn = Dmak.prototype = {
-        
-        setStrokes: function(strokes) {
+
+        setStrokes: function (strokes) {
             this.strokes = preprocessStrokes(strokes);
-            this.papers   = giveBirthToRaphael(strokes.length);
-            if(this.options.grid.show) {
+            this.papers = giveBirthToRaphael(strokes.length);
+            if (this.options.grid.show) {
                 showGrid(this.papers);
             }
         },
@@ -70,19 +73,19 @@
         /**
          * Clean all strokes on papers.
          */
-        erase: function(end) {
+        erase: function (end) {
             // Cannot have two rendering process for
             // the same draw. Keep it cool.
-            if(this.timeouts.length) {
+            if (this.timeouts.length) {
                 return false;
             }
 
             // Don't go behind the beginning.
-            if(this.pointer <= 0) {
+            if (this.pointer <= 0) {
                 return false;
             }
 
-            if(typeof end === "undefined") {
+            if (typeof end === "undefined") {
                 end = 0;
             }
 
@@ -92,29 +95,28 @@
                 this.strokes[this.pointer].object = null;
                 this.options.erased(this.pointer);
             }
-            while(this.pointer > end)
+            while (this.pointer > end);
         },
 
         /**
          * All the magic happens here.
          */
-        render: function(end) {
+        render: function (end) {
 
             // Cannot have two rendering process for
             // the same draw. Keep it cool.
-            if(this.timeouts.length) {
+            if (this.timeouts.length) {
                 return false;
             }
-        
-            if(typeof end === "undefined") {
+
+            if (typeof end === "undefined") {
                 end = this.strokes.length;
-            }
-            else if(end > this.strokes.length) {
+            } else if (end > this.strokes.length) {
                 return false;
             }
 
             var delay = 0;
-            var func = function(that) {
+            var func = function (that) {
                 createStroke(that.papers[that.strokes[that.pointer].char], that.strokes[that.pointer]);
                 that.pointer++;
                 that.timeouts.shift();
@@ -122,10 +124,9 @@
             };
 
             for (var i = this.pointer; i < end; i++) {
-                if(delay <= 0) {
+                if (delay <= 0) {
                     func(this);
-                }
-                else {
+                } else {
                     var t = setTimeout(func, delay, this);
                     this.timeouts.push(t);
                 }
@@ -136,7 +137,7 @@
         /**
          * Pause rendering
          */
-        pause: function(end) {
+        pause: function () {
             for (var i = 0; i < this.timeouts.length; i++) {
                 window.clearTimeout(this.timeouts[i]);
             }
@@ -146,14 +147,14 @@
         /**
          * Wrap the erase function to remove the x last strokes.
          */
-        eraseLastStrokes: function(nbStrokes){
+        eraseLastStrokes: function (nbStrokes) {
             this.erase(this.pointer - nbStrokes);
         },
 
         /**
          * Wrap the render function to render the x next strokes.
          */
-        renderNextStrokes: function(nbStrokes){
+        renderNextStrokes: function (nbStrokes) {
             this.render(this.pointer + nbStrokes);
         }
 
@@ -168,67 +169,66 @@
      *  - Maps to a character index
      *  - Calculates path length
      */
-    var preprocessStrokes = function(strokes) {
+    var preprocessStrokes = function (strokes) {
         var s = [];
-        for(var i = 0; i < strokes.length; i++) {
+        for (var i = 0; i < strokes.length; i++) {
             for (var j = 0; j < strokes[i].length; j++) {
                 var length = Raphael.getTotalLength(strokes[i][j]);
                 var stroke = {
-                    'char' : i,
-                    'length' : length,
-                    'duration' : length * Dmak.options.step * 1000,
-                    'path' : strokes[i][j],
-                    'object' : null
-                }
+                    'char': i,
+                    'length': length,
+                    'duration': length * Dmak.options.step * 1000,
+                    'path': strokes[i][j],
+                    'object': null
+                };
                 s.push(stroke);
             }
         }
 
         return s;
-    }
+    };
 
     /**
      * Init Raphael paper objects
      */
-    var giveBirthToRaphael = function(nbChar) {
-        var papers = []
-        for(var i = 0; i < nbChar; i++) {
-            var paper = new Raphael(Dmak.options.element, Dmak.options.width + "px", Dmak.options.height +"px");
-            paper.canvas.setAttribute("class","dmak-svg");
+    var giveBirthToRaphael = function (nbChar) {
+        var papers = [];
+        for (var i = 0; i < nbChar; i++) {
+            var paper = new Raphael(Dmak.options.element, Dmak.options.width + "px", Dmak.options.height + "px");
+            paper.canvas.setAttribute("class", "dmak-svg");
             papers.push(paper);
         }
-
         return papers.reverse();
-    }
+    };
 
     /**
      * Draw the background grid
      */
-    var showGrid = function(papers) {
-        for(var i = 0; i < papers.length; i++) {
+    var showGrid = function (papers) {
+        for (var i = 0; i < papers.length; i++) {
             papers[i].path("M" + (Dmak.options.width / 2) + ",0 L" + (Dmak.options.width / 2) + "," + Dmak.options.height).attr(Dmak.options.grid.attr);
             papers[i].path("M0," + (Dmak.options.height / 2) + " L" + Dmak.options.width + "," + (Dmak.options.height / 2)).attr(Dmak.options.grid.attr);
         }
-    }
+    };
 
     /**
      * Draw a single stroke ; drawing can be animated if set as so.
      */
-    var createStroke = function(paper, stroke) {
+    var createStroke = function (paper, stroke) {
         stroke.object = paper.path(stroke.path);
         stroke.object.attr(Dmak.options.stroke.attr);
-        if(Dmak.options.stroke.animated) {
+        if (Dmak.options.stroke.animated) {
             animateStroke(stroke);
         }
-    }
+    };
 
     /**
      * Animate stroke drawing.
      * Based on the great article wrote by Jake Archibald
      * http://jakearchibald.com/2013/animated-line-drawing-svg/
      */
-    var animateStroke = function(stroke) {
-        stroke.object.attr({'stroke' : Dmak.options.stroke.attr.active});
+    var animateStroke = function (stroke) {
+        stroke.object.attr({'stroke': Dmak.options.stroke.attr.active});
         stroke.object.node.style.transition = stroke.object.node.style.WebkitTransition = 'none';
         // Set up the starting positions
         stroke.object.node.style.strokeDasharray = stroke.length + ' ' + stroke.length;
@@ -240,10 +240,9 @@
         // Go!
         stroke.object.node.style.strokeDashoffset = '0';
         // Revert back to the options color when the animation is done.
-        setTimeout(function(){
-            // The stroke object may have been already erased when we reach this
-            // timeout
-            if(stroke.object == null) {
+        setTimeout(function () {
+            // The stroke object may have been already erased when we reach this timeout
+            if (stroke.object === null) {
                 return;
             }
             stroke.object.node.style.stroke = Dmak.options.stroke.attr.stroke;
@@ -255,20 +254,17 @@
      * Simplistic helper function for extending objects
      */
     var extend = function (defaults, replacement) {
-        if(arguments.length != 2) {
+        if (arguments.length != 2) {
             throw new Error('Missing arguments in extend function');
         }
-
         var result = defaults;
-        for(var key in replacement) {
-            if(typeof result[key] === 'object') {
+        for (var key in replacement) {
+            if (typeof result[key] === 'object') {
                 result[key] = extend(result[key], replacement[key]);
-            } else if(result.hasOwnProperty(key)) {
+            } else if (result.hasOwnProperty(key)) {
                 result[key] = replacement[key];
             }
         }
-        
-        
         return result;
     };
 
--- src/dmakLoader.js
+++ src/dmakLoader.js
@@ -1,31 +1,29 @@
  ;(function () {
 
-    "use strict"
+    "use strict";
 
     // Create a safe reference to the DrawMeAKanji object for use below.
-    var DmakLoader = function(uri) {
-        this.uri     = uri;
+    var DmakLoader = function (uri) {
+        this.uri = uri;
     };
 
     // Gather SVG data information for a given set of characters.
     // By default this action is done while instanciating the Word
-    // object, but it can be skipped, see above 
+    // object, but it can be skipped, see above
     DmakLoader.prototype.load = function (text, callback) {
-        var done = 0;
         var paths = [];
         var nbChar = text.length;
         var done = 0;
-        for(var i = 0; i < nbChar; i++ ) {
+        for (var i = 0; i < nbChar; i++) {
             loadSvg(this.uri, i, text.charCodeAt(i).toString(16), {
-
-                done: function(index, data) {
+                done: function (index, data) {
                     paths[index] = data;
                     done++;
-                    if(done === nbChar){
+                    if (done === nbChar) {
                         callback(paths);
                     }
                 },
-                error: function(msg) {
+                error: function (msg) {
                     console.log('Error', msg);
                 }
             });
@@ -33,22 +31,22 @@
     };
 
     // Try to load a SVG file matching the given char code.
-    // @thanks to the incredible work made by KanjiVG 
+    // @thanks to the incredible work made by KanjiVG
     // @see: http://kanjivg.tagaini.net
     function loadSvg(uri, index, charCode, callbacks) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", uri + "0" + charCode + ".svg", true);  
-        xhr.onreadystatechange = function () {  
-            if (xhr.readyState === 4) {  
-                if (xhr.status === 200) {  
+        xhr.open("GET", uri + "0" + charCode + ".svg", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
                     callbacks.done(index, parseResponse(xhr.response));
                 } else {
                     callbacks.error(xhr.statusText);
-                }  
-            }  
-        };  
+                }
+            }
+        };
         xhr.send();
-    };
+    }
 
     // Parse the SVG data to only keep paths data.
     function parseResponse(response) {
@@ -57,7 +55,7 @@
         var paths = [];
         for (var i = 0; i < elements.length; i++) {
             paths.push(elements[i].getAttribute('d'));
-        };
+        }
         return paths;
     }
 
