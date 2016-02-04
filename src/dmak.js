6d15724ab7883dc0b0285a844e1f147def09f281
konqi <elkonqi@gmail.com>
2016-02-04 19:18:49 +0100

    - allow multiple Dmak objects to work in parallel by not using class variables in helper methods

--- src/dmak.js
+++ src/dmak.js
@@ -1,4 +1,4 @@
- ;(function () {
+;(function () {
 
 	"use strict";
 
@@ -9,9 +9,9 @@
 		this.papers = [];
 		this.pointer = 0;
 		this.timeouts = {
-			play : [],
-			erasing : [],
-			drawing : []
+			play: [],
+			erasing: [],
+			drawing: []
 		};
 
 		if (!this.options.skipLoad) {
@@ -49,9 +49,9 @@
 		step: 0.03,
 		element: "draw",
 		stroke: {
-			animated : {
-				drawing : true,
-				erasing : true
+			animated: {
+				drawing: true,
+				erasing: true
 			},
 			order: {
 				visible: false,
@@ -91,10 +91,10 @@
 		 * Prepare kanjis and papers for rendering.
 		 */
 		prepare: function (data) {
-			this.kanjis = preprocessStrokes(data);
-			this.papers = giveBirthToRaphael(data.length);
+			this.kanjis = this.preprocessStrokes(data);
+			this.papers = this.giveBirthToRaphael(data.length);
 			if (this.options.grid.show) {
-				showGrid(this.papers);
+				this.showGrid(this.papers);
 			}
 		},
 
@@ -118,7 +118,7 @@
 
 			do {
 				this.pointer--;
-				eraseStroke(this.kanjis[this.pointer], this.timeouts.erasing);
+				this.eraseStroke(this.kanjis[this.pointer], this.timeouts.erasing);
 
 				// Execute custom callback "erased" here
 				this.options.erased(this.pointer);
@@ -143,15 +143,15 @@
 				return false;
 			}
 
-			var cb = function (that) {
-					drawStroke(that.papers[that.kanjis[that.pointer].char], that.kanjis[that.pointer], that.timeouts.drawing);
+			var cb = function () {
+					this.drawStroke(this.papers[this.kanjis[this.pointer].char], this.kanjis[this.pointer], this.timeouts.drawing);
 
 					// Execute custom callback "drew" here
-					that.options.drew(that.pointer);
+					this.options.drew(this.pointer);
 
-					that.pointer++;
-					that.timeouts.play.shift();
-				},
+					this.pointer++;
+					this.timeouts.play.shift();
+				}.bind(this),
 				delay = 0,
 				i;
 
@@ -162,10 +162,10 @@
 			}
 
 			for (i = this.pointer; i < end; i++) {
-				if (!Dmak.options.stroke.animated.drawing || delay <= 0) {
+				if (!this.options.stroke.animated.drawing || delay <= 0) {
 					cb(this);
 				} else {
-					this.timeouts.play.push(setTimeout(cb, delay, this));
+					this.timeouts.play.push(setTimeout(cb, delay));
 				}
 				delay += this.kanjis[i].duration;
 			}
@@ -193,179 +193,178 @@
 		 */
 		renderNextStrokes: function (nbStrokes) {
 			this.render(this.pointer + nbStrokes);
-		}
+		},
 
-	};
+		// HELPERS
 
-	// HELPERS
+		/**
+		 * Flattens the array of strokes ; 3D > 2D and does some preprocessing while
+		 * looping through all the strokes:
+		 *  - Maps to a character index
+		 *  - Calculates path length
+		 */
+		preprocessStrokes: function (data) {
+			var strokes = [],
+				stroke,
+				length,
+				i,
+				j;
 
-	/**
-	 * Flattens the array of strokes ; 3D > 2D and does some preprocessing while
-	 * looping through all the strokes:
-	 *  - Maps to a character index
-	 *  - Calculates path length
-	 */
-	function preprocessStrokes(data) {
-		var strokes = [],
-			stroke,
-			length,
-			i,
-			j;
-
-		for (i = 0; i < data.length; i++) {
-			for (j = 0; j < data[i].length; j++) {
-				length = Raphael.getTotalLength(data[i][j].path);
-				stroke = {
-					"char": i,
-					"length": length,
-					"duration": length * Dmak.options.step * 1000,
-					"path": data[i][j].path,
-					"groups" : data[i][j].groups,
-					"text": data[i][j].text,
-					"object": {
-						"path" : null,
-						"text": null
-					}
-				};
-				strokes.push(stroke);
+			for (i = 0; i < data.length; i++) {
+				for (j = 0; j < data[i].length; j++) {
+					length = Raphael.getTotalLength(data[i][j].path);
+					stroke = {
+						"char": i,
+						"length": length,
+						"duration": length * this.options.step * 1000,
+						"path": data[i][j].path,
+						"groups": data[i][j].groups,
+						"text": data[i][j].text,
+						"object": {
+							"path": null,
+							"text": null
+						}
+					};
+					strokes.push(stroke);
+				}
 			}
-		}
 
-		return strokes;
-	}
+			return strokes;
+		},
 
-	/**
-	 * Init Raphael paper objects
-	 */
-	function giveBirthToRaphael(nbChar) {
-		var papers = [],
-			paper,
-			i;
+		/**
+		 * Init Raphael paper objects
+		 */
+		giveBirthToRaphael: function (nbChar) {
+			var papers = [],
+				paper,
+				i;
 
-		for (i = 0; i < nbChar; i++) {
-			paper = new Raphael(Dmak.options.element, Dmak.options.width + "px", Dmak.options.height + "px");
-			paper.setViewBox(Dmak.options.viewBox.x, Dmak.options.viewBox.y, Dmak.options.viewBox.w, Dmak.options.viewBox.h);
-			paper.canvas.setAttribute("class", "dmak-svg");
-			papers.push(paper);
-		}
-		return papers.reverse();
-	}
+			for (i = 0; i < nbChar; i++) {
+				paper = new Raphael(this.options.element, this.options.width + "px", this.options.height + "px");
+				paper.setViewBox(this.options.viewBox.x, this.options.viewBox.y, this.options.viewBox.w, this.options.viewBox.h);
+				paper.canvas.setAttribute("class", "dmak-svg");
+				papers.push(paper);
+			}
+			return papers.reverse();
+		},
 
-	/**
-	 * Draw the background grid
-	 */
-	function showGrid(papers) {
-		var i;
+		/**
+		 * Draw the background grid
+		 */
+		showGrid: function (papers) {
+			var i;
 
-		for (i = 0; i < papers.length; i++) {
-			papers[i].path("M" + (Dmak.options.viewBox.w / 2) + ",0 L" + (Dmak.options.viewBox.w / 2) + "," + Dmak.options.viewBox.h).attr(Dmak.options.grid.attr);
-			papers[i].path("M0," + (Dmak.options.viewBox.h / 2) + " L" + Dmak.options.viewBox.w + "," + (Dmak.options.viewBox.h / 2)).attr(Dmak.options.grid.attr);
-		}
-	}
+			for (i = 0; i < papers.length; i++) {
+				papers[i].path("M" + (this.options.viewBox.w / 2) + ",0 L" + (this.options.viewBox.w / 2) + "," + this.options.viewBox.h).attr(this.options.grid.attr);
+				papers[i].path("M0," + (this.options.viewBox.h / 2) + " L" + this.options.viewBox.w + "," + (this.options.viewBox.h / 2)).attr(this.options.grid.attr);
+			}
+		},
 
-	/**
-	 * Remove a single stroke ; deletion can be animated if set as so.
-	 */
-	function eraseStroke(stroke, timeouts) {
-		// In some cases the text object may be null:
-		//  - Stroke order display disabled
-		//  - Stroke already deleted
-		if (stroke.object.text !== null) {
-			stroke.object.text.remove();
-		}
+		/**
+		 * Remove a single stroke ; deletion can be animated if set as so.
+		 */
+		eraseStroke: function (stroke, timeouts) {
+			// In some cases the text object may be null:
+			//  - Stroke order display disabled
+			//  - Stroke already deleted
+			if (stroke.object.text !== null) {
+				stroke.object.text.remove();
+			}
 
-		var cb = function() {
-			stroke.object.path.remove();
+			var cb = function () {
+				stroke.object.path.remove();
 
-			// Finally properly prepare the object variable
-			stroke.object = {
-				"path" : null,
-				"text" : null
+				// Finally properly prepare the object variable
+				stroke.object = {
+					"path": null,
+					"text": null
+				};
+
+				timeouts.shift();
 			};
 
-			timeouts.shift();
-		};
+			if (this.options.stroke.animated.erasing) {
+				stroke.object.path.node.style.stroke = this.options.stroke.attr.active;
+				timeouts.push(this.animateStroke(stroke, -1, cb));
+			}
+			else {
+				cb();
+			}
+		},
 
-		if (Dmak.options.stroke.animated.erasing) {
-			stroke.object.path.node.style.stroke = Dmak.options.stroke.attr.active;
-			timeouts.push(animateStroke(stroke, -1, cb));
-		}
-		else {
-			cb();
-		}
-	}
+		/**
+		 * Draw a single stroke ; drawing can be animated if set as so.
+		 */
+		drawStroke: function (paper, stroke, timeouts) {
+			var cb = function () {
 
-	/**
-	 * Draw a single stroke ; drawing can be animated if set as so.
-	 */
-	function drawStroke(paper, stroke, timeouts) {
-		var cb = function() {
+				// The stroke object may have been already erased when we reach this timeout
+				if (stroke.object.path === null) {
+					return;
+				}
 
-			// The stroke object may have been already erased when we reach this timeout
-			if (stroke.object.path === null) {
-				return;
+				var color = this.options.stroke.attr.stroke;
+				if (this.options.stroke.attr.stroke === "random") {
+					color = Raphael.getColor();
+				}
+
+				// Revert back to the default color.
+				stroke.object.path.node.style.stroke = color;
+				stroke.object.path.node.style.transition = stroke.object.path.node.style.WebkitTransition = "stroke 400ms ease";
+
+				timeouts.shift();
+			}.bind(this);
+
+			stroke.object.path = paper.path(stroke.path);
+			stroke.object.path.attr(this.options.stroke.attr);
+
+			if (this.options.stroke.order.visible) {
+				this.showStrokeOrder(paper, stroke);
 			}
 
-			var color = Dmak.options.stroke.attr.stroke;
-			if(Dmak.options.stroke.attr.stroke === "random") {
-				color = Raphael.getColor();
+			if (this.options.stroke.animated.drawing) {
+				this.animateStroke(stroke, 1, cb);
 			}
+			else {
+				cb();
+			}
+		},
 
-			// Revert back to the default color.
-			stroke.object.path.node.style.stroke = color;
-			stroke.object.path.node.style.transition = stroke.object.path.node.style.WebkitTransition = "stroke 400ms ease";
+		/**
+		 * Draw a single next to
+		 */
+		showStrokeOrder: function (paper, stroke) {
+			stroke.object.text = paper.text(stroke.text.x, stroke.text.y, stroke.text.value);
+			stroke.object.text.attr(this.options.stroke.order.attr);
+		},
 
-			timeouts.shift();
-		};
+		/**
+		 * Animate stroke drawing.
+		 * Based on the great article wrote by Jake Archibald
+		 * http://jakearchibald.com/2013/animated-line-drawing-svg/
+		 */
+		animateStroke: function (stroke, direction, callback) {
+			stroke.object.path.attr({"stroke": this.options.stroke.attr.active});
+			stroke.object.path.node.style.transition = stroke.object.path.node.style.WebkitTransition = "none";
 
-		stroke.object.path = paper.path(stroke.path);
-		stroke.object.path.attr(Dmak.options.stroke.attr);
+			// Set up the starting positions
+			stroke.object.path.node.style.strokeDasharray = stroke.length + " " + stroke.length;
+			stroke.object.path.node.style.strokeDashoffset = (direction > 0) ? stroke.length : 0;
 
-		if (Dmak.options.stroke.order.visible) {
-			showStrokeOrder(paper, stroke);
+			// Trigger a layout so styles are calculated & the browser
+			// picks up the starting position before animating
+			stroke.object.path.node.getBoundingClientRect();
+			stroke.object.path.node.style.transition = stroke.object.path.node.style.WebkitTransition = "stroke-dashoffset " + stroke.duration + "ms ease";
+
+			// Go!
+			stroke.object.path.node.style.strokeDashoffset = (direction > 0) ? "0" : stroke.length;
+
+			// Execute the callback once the animation is done
+			// and return the timeout id.
+			return setTimeout(callback, stroke.duration);
 		}
-
-		if (Dmak.options.stroke.animated.drawing) {
-			animateStroke(stroke, 1, cb);
-		}
-		else {
-			cb();
-		}
-	}
-
-	/**
-	 * Draw a single next to
-	 */
-	function showStrokeOrder(paper, stroke) {
-		stroke.object.text = paper.text(stroke.text.x, stroke.text.y, stroke.text.value);
-		stroke.object.text.attr(Dmak.options.stroke.order.attr);
-	}
-
-	/**
-	 * Animate stroke drawing.
-	 * Based on the great article wrote by Jake Archibald
-	 * http://jakearchibald.com/2013/animated-line-drawing-svg/
-	 */
-	function animateStroke(stroke, direction, callback) {
-		stroke.object.path.attr({"stroke": Dmak.options.stroke.attr.active});
-		stroke.object.path.node.style.transition = stroke.object.path.node.style.WebkitTransition = "none";
-
-		// Set up the starting positions
-		stroke.object.path.node.style.strokeDasharray = stroke.length + " " + stroke.length;
-		stroke.object.path.node.style.strokeDashoffset = (direction > 0) ? stroke.length : 0;
-
-		// Trigger a layout so styles are calculated & the browser
-		// picks up the starting position before animating
-		stroke.object.path.node.getBoundingClientRect();
-		stroke.object.path.node.style.transition = stroke.object.path.node.style.WebkitTransition = "stroke-dashoffset " + stroke.duration + "ms ease";
-
-		// Go!
-		stroke.object.path.node.style.strokeDashoffset = (direction > 0) ? "0" : stroke.length;
-
-		// Execute the callback once the animation is done
-		// and return the timeout id.
-		return setTimeout(callback, stroke.duration);
-	}
+	};
 
 	/**
 	 * Simplistic helper function for extending objects
